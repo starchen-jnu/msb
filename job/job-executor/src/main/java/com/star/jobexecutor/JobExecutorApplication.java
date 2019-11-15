d10071c7d6d131ab35ecb654357a176478ce9d89
star <619321234@163.com>
2019-11-15 15:29:18 +0800

    maven

---
+++ job/job-executor/src/main/java/com/star/jobexecutor/JobExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.star.jobexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobExecutorApplication.class, args);
+	}
+
+}
---
+++ job/job-executor/src/test/java/com/star/jobexecutor/JobExecutorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.star.jobexecutor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobExecutorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
